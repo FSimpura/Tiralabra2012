a932b4390b887da5daaa1f8204561991bfa47e61
Frans Simpura <FS@FS-PC.(none)>
2012-05-25 20:30:44 +0300

    Heuristics for the minimax function

--- cnai.py
+++ cnai.py
@@ -7,33 +7,95 @@ import sys
 
 class cnai:
 
-    def _init__(self, skillLevel):
+    def __init__(self, skillLevel):
         self.skillLevel = skillLevel
-    
+
+    def tokenCountWeighted(self, n):
+        if n == 0:
+            return 0
+        return pow(2, (n-1)*2)
+        
     # Sums up all the heuristics of given move
     def moveTotalValue(self, move, board):
-        return diagonalValue(move, board) + horizontalValue(move, board) + verticalValue(move, board) 
+        return self.decDiagonalValue(move, board) + self.incDiagonalValue(move, board) + self.horizontalValue(move, board) + self.verticalValue(move, board) 
 
-    # TODO: Calculates the diagonal heuristics of given move
-    def diagonalValue(self, move, board):
-        pass
+    # Calculates the diagonal (increasing) heuristics of given move
+    def incDiagonalValue(self, move, board):
+        heuristics = 0
+        tokenCount = 0
+        for offset in range(-board.toWin+1, board.toWin): # Define the offset according to the win condition
+            for step in range(board.toWin):
+                if not board.inBounds(move.x + offset + step, move.y + offset + step):
+ 
+                    tokenCount = 0
+                    break # Out-of-bounds; winning condition will not be met
+                if board.getBoard()[move.x + offset + step][move.y + offset + step] == move.token:
+                   tokenCount += 1
+                elif board.getBoard()[move.x + offset + step][move.y + offset + step] != None:
+                    tokenCount = 0
+                    break
+            heuristics += self.tokenCountWeighted(tokenCount)
+            tokenCount = 0
+        return heuristics
         
-    # TODO: Calculates the horizontal heuristics of given move
-    def horizontalValue(self, move, board):
-        pass
-        
-    # TODO: Calculates the vertical heuristics of given move   
+    def decDiagonalValue(self, move, board):
+        heuristics = 0
+        tokenCount = 0
+        for offset in range(-board.toWin+1, board.toWin): # Define the offset according to the win condition
+            for step in range(board.toWin):
+                if not board.inBounds(move.x - offset - step, move.y + offset + step):
+                    tokenCount = 0
+                    break # Out-of-bounds; winning condition will not be met
+                if board.getBoard()[move.x - offset - step][move.y + offset + step] == move.token:
+                   tokenCount += 1
+                elif board.getBoard()[move.x - offset - step][move.y + offset + step] != None:
+                    tokenCount = 0
+                    break
+            heuristics += self.tokenCountWeighted(tokenCount)
+            tokenCount = 0
+        return heuristics  
+    # Calculates the vertical heuristics of given move
     def verticalValue(self, move, board):
-        pass
+        heuristics = 0
+        tokenCount = 0
+        for offset in range(-board.toWin+1, board.toWin): # Define the offset according to the win condition
+            for step in range(board.toWin):
+                if not board.inBounds(move.x + offset + step, move.y):
+                    break # Out-of-bounds; winning condition will not be met
+                if board.getBoard()[move.x + offset + step][move.y] == move.token:
+                   tokenCount += 1
+                elif board.getBoard()[move.x + offset + step][move.y] != None:
+                    tokenCount = 0
+                    break
+            heuristics += self.tokenCountWeighted(tokenCount)
+            tokenCount = 0
+        return heuristics
+        
+    # Calculates the horizontal heuristics of given move   
+    def horizontalValue(self, move, board):
+        heuristics = 0
+        tokenCount = 0
+        for offset in range(-board.toWin+1, board.toWin): # Define the offset according to the win condition
+            for step in range(board.toWin):
+                if not board.inBounds(move.x, move.y + offset + step):
+                    tokenCount = 0
+                    break # Out-of-bounds; winning condition will not be met
+                if board.getBoard()[move.x][move.y + offset + step] == move.token:
+                   tokenCount += 1
+                elif board.getBoard()[move.x][move.y + offset + step] != None:
+                    tokenCount = 0
+                    break
+            heuristics += self.tokenCountWeighted(tokenCount)
+            tokenCount = 0
+        return heuristics
 	
     # TODO: Calculates the best move according to the skillLevel using minimax algorithm	
-    def minimax(self, move, depth): 
+    def minimax(self, move, depth, board): 
         if self.isWinningMove(move.x, move.y):
             return sys.maxint
 
         if depth == 0:
-            # TODO: "return the heuristic value of the current move"
-            pass
+            return self.moveTotalValue(move, board)
 
         alpha = sys.maxint
 
@@ -137,3 +199,15 @@ class Board:
         if self.inBounds(x, y):
             return self.board[x][y]
         return None
+        
+# Basic board printing, for testing
+def printAsBoard(board):
+    for row in range(board.LAST_ROW, -1, -1):
+        for column in range(0, board.LAST_COLUMN+1):
+            cell = board.getBoard()[row][column]
+            if cell == None:
+                print ("."),
+            else:
+                print(cell),
+        print("")
+    print("")
